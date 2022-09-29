50a538ce350ee32bd95fa2196fae6e9709ce6416
range.chen <range.chen@quectel.com>
2022-09-29 19:38:25 +0800

    CreateYourProject.bat is used for creating your own project from a template

---
+++ CreateYourProject.bat
@@ -0,0 +1,23 @@
+echo "if you install template error,please email to 'feihuazhuyu@126.com'"
+
+dotnet new -i Wpf.MvvmLight.SelfHost.Template
+
+set /p OP=Please set your project name(for example:Range.Demo):
+
+md .YourProject
+
+cd .YourProject
+
+dotnet new wmstpl -n %OP%
+
+cd ../
+
+
+echo "Create project on yourself successfully!!!! ^ please see the folder .YourProject"
+
+dotnet new -u Wpf.MvvmLight.SelfHost.Template
+
+
+echo "Delete Template Successfully"
+
+pause
